7bcb2beabd6dbad05852dabb3b9f065744914f0d
Jan Svatoň <450402@mail.muni.cz>
2022-05-25 09:06:57 +0200

    Edited configs for k8s

--- run.sh
+++
@@ -1,4 +0,0 @@
-nextflow kuberun xsvato01/mareckova_CXCR_k8s/ -pod-image 'cerit.io/nextflow:21.09.1' \
-        -w /mnt/home/450402/000000-My_Documents/mareckova_k8s_work/tmp \
-        -v pvc-janek-storage-elixir1-cerit-sc-cz:/mnt -c nextflow.config \
-        --datain /mnt/home/450402/000000-My_Documents/mareckova_data/
