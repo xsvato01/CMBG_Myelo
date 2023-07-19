0ceb406d406ef771a32a8fbc86fb379614e98957
Jan Svatoñ <janek@keepers-iMac.local>
2023-07-19 16:17:31 +0200

    clean up after long time, new nextflow head image

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nextflow kuberun xsvato01/mareckova_CXCR_k8s -pod-image 'cerit.io/nextflow/nextflow:22.06.1' -resume  -c zaloha_nextflow.config --datain /mnt/shared/MedGen/sequencing_results/primary_data/230106_TP53_20230106
+nextflow kuberun xsvato01/mareckova_CXCR_k8s  -head-image 'cerit.io/nextflow/nextflow:22.11.1' -resume --datain /mnt/shared/MedGen/sequencing_results/primary_data/230106_TP53_20230106
